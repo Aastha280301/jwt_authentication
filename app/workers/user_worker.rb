6d084d3bff0fcb8517ed9bc6c4be16e54c1df6b7
Aastha280301 <aamujawdiya@bestpeers.com>
2023-10-06 17:19:59 +0530

    "Reset password api"

--- app/workers/user_worker.rb
+++ app/workers/user_worker.rb
@@ -2,14 +2,19 @@ require 'csv'
 
 class UserWorker
   include Sidekiq::Worker
-
+  attr_accessor :csv_path
   def perform(csv_path)
-    CSV.foreach(csv_path, headers: true) do |user|
-      User.create!(
-        username: user[1],
-        email: user[2],
-       password: "aastha",
-      )
-    end
+    @csv_path = csv_path
+    create_users  
   end
+
+ def create_users
+    CSV.foreach(csv_path, headers: true) do |user|
+    User.create!(
+      username: user['username'],
+      email: user['email'],
+      password: "aastha",
+    )
+  end
+end
 end
