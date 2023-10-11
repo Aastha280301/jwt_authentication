54e7bd10de6ae2a9d12e66bfdd0d3421a88561f3
Aastha280301 <aamujawdiya@bestpeers.com>
2023-10-11 11:30:07 +0530

    "Search API implementation"

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,17 @@
 class OrdersController < ApplicationController
+
   def search
     @search_product = params[:search_product]
     @search_user = params[:search_user]
 
-    product = Product.find_by(products_name: "#{@search_product}")
-    user = Product.find_by(user_id: "#{@search_user}")
-    
-    if product
-      order = Order.find_by(id: product.order_id)
-      render json: order
-    elsif user
-      order = Order.find_by(id: user.order_id)
+    order_data = Product.find_by(products_name: "#{@search_product}") || Product.find_by(user_id:  "#{@search_user}")
+
+    if order_data
+      order = Order.find_by(id: order_data.order_id)
       render json: order
     else
       render json: { errors: "Enter valid information"}
     end
-
   end
+
 end
