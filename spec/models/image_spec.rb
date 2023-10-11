f4d3393ea03bcf2e4a2e117df6e43507e0b9cdc4
Aastha280301 <aamujawdiya@bestpeers.com>
2023-10-11 13:09:00 +0530

    "Add_another_approach_polymorphic"

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Image, type: :model do
   describe 'Associations' do
-    it 'belongs to imageable polymorphically' do
-      expect(Image.reflect_on_association(:imageable).macro).to eq :belongs_to
-      expect(Image.reflect_on_association(:imageable).options[:polymorphic]).to be true
+    it "has a polymorphic relationship" do
+      expect(subject).to belong_to(:imageable)
     end
   end
 end
